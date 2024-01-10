202f7f2c0198a8b5200fa990a48c577c01809000
Yu-Chien (Andie) Chang <12643937+andiechang77@users.noreply.github.com>
2024-01-09 23:42:39 -0500

    image optimized & add favicon

--- next.config.js
+++ next.config.js
@@ -4,22 +4,12 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-   */
-  output: "export",
-
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
-   */
-  images: {
-    unoptimized: true,
-  },
+  //   /**
+  //    * Enable static exports for the App Router.
+  //    *
+  //    * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  //    */
+  //   output: "export",
 };
 
 module.exports = nextConfig;
